0c20e912f2b3772bc55a94385e0a849d5330692c
Don-Stevenson <dsteven1@gmail.com>
2019-10-23 20:29:06 +0000

    now contains the 2 functions from play.js

---
+++ input.js
@@ -0,0 +1,19 @@
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on("data", handleUserInput)
+  return stdin;
+}
+
+const handleUserInput = function(key){
+   if  (key === '\u0003'){
+     console.log("exiting the game now");
+     process.exit();
+  } else if (key === "w"){conn.write("Move: up")
+  } else if (key === "s"){conn.write("Move: down")
+  } else if (key === "d"){conn.write("Move: left")
+  } else if (key === "a"){conn.write("Move: right")
+}
+module.exports = setupInput;
