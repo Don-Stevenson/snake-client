b7bc1c33768c44c27fc74dc3db5a7cdc9bfc4116
Don-Stevenson <dsteven1@gmail.com>
2019-10-23 20:28:28 +0000

    refactored and put the 2 functions into input.js

--- play.js
+++ play.js
@@ -1,29 +1,14 @@
 
 const connect = require('./client');
+const setupInput = require('./input');
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on("data", handleUserInput)
-  return stdin;
-}
-
-const handleUserInput = function(key){
-   if  (key === '\u0003'){
-     console.log("exiting the game now");
-     process.exit();
-  } else if (key === "w"){conn.write("Move: up")
-  } else if (key === "s"){conn.write("Move: down")
-  } else if (key === "d"){conn.write("Move: left")
-  } else if (key === "a"){conn.write("Move: right")
-}
 /**
  * Establishes connection with the game server
  */
 
-setupInput();
+
 
 console.log('Connecting ...');
 connect();
+
+setupInput();
